70ef54a58d369849de71f4339fde9a25581cc3d9
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-05-25 19:48:32 -0400

    add display name to status

--- src/main/java/rainbowbismuth/fft/Status.java
+++ src/main/java/rainbowbismuth/fft/Status.java
@@ -1,51 +1,57 @@
 package rainbowbismuth.fft;
 
 public enum Status {
-    CRYSTAL(0, 0x40),
-    DEAD(0, 0x20),
-    CHARGING(0, 0x08),
-    JUMP(0, 0x04),
-    DEFENDING(0, 0x02),
-    PERFORMING(0, 0x01),
-    PETRIFY(1, 0x80),
-    INVITE(1, 0x40),
-    DARKNESS(1, 0x20),
-    CONFUSION(1, 0x10),
-    SILENCE(1, 0x08),
-    BLOOD_SUCK(1, 0x04),
-    CURSED(1, 0x02),
-    TREASURE(1, 0x01),
-    OIL(2, 0x80),
-    FLOAT(2, 0x40),
-    RERAISE(2, 0x20),
-    TRANSPARENT(2, 0x10),
-    BERSERK(2, 0x08),
-    CHICKEN(2, 0x04),
-    FROG(2, 0x02),
-    CRITICAL(2, 0x01),
-    POISON(3, 0x80),
-    REGEN(3, 0x40),
-    PROTECT(3, 0x20),
-    SHELL(3, 0x10),
-    HASTE(3, 0x08),
-    SLOW(3, 0x04),
-    STOP(3, 0x02),
-    WALL(3, 0x01),
-    FAITH(4, 0x80),
-    INNOCENT(4, 0x40),
-    CHARM(4, 0x20),
-    SLEEP(4, 0x10),
-    DONT_MOVE(4, 0x08),
-    DONT_ACT(4, 0x04),
-    REFLECT(4, 0x02),
-    DEATH_SENTENCE(4, 0x01);
+    CRYSTAL("Crystal", 0, 0x40),
+    DEAD("Dead", 0, 0x20),
+    CHARGING("Charging", 0, 0x08),
+    JUMP("Jump", 0, 0x04),
+    DEFENDING("Defending", 0, 0x02),
+    PERFORMING("Performing", 0, 0x01),
+    PETRIFY("Petrify", 1, 0x80),
+    INVITE("Invite", 1, 0x40),
+    DARKNESS("Darkness", 1, 0x20),
+    CONFUSION("Confusion", 1, 0x10),
+    SILENCE("Silence", 1, 0x08),
+    BLOOD_SUCK("Blood Suck", 1, 0x04),
+    CURSED("Cursed", 1, 0x02),
+    TREASURE("Treasure", 1, 0x01),
+    OIL("Oil", 2, 0x80),
+    FLOAT("Float", 2, 0x40),
+    RERAISE("Reraise", 2, 0x20),
+    TRANSPARENT("Transparent", 2, 0x10),
+    BERSERK("Berserk", 2, 0x08),
+    CHICKEN("Chicken", 2, 0x04),
+    FROG("Frog", 2, 0x02),
+    CRITICAL("Critical", 2, 0x01),
+    POISON("Poison", 3, 0x80),
+    REGEN("Regen", 3, 0x40),
+    PROTECT("Protect", 3, 0x20),
+    SHELL("Shell", 3, 0x10),
+    HASTE("Haste", 3, 0x08),
+    SLOW("Slow", 3, 0x04),
+    STOP("Stop", 3, 0x02),
+    WALL("Wall", 3, 0x01),
+    FAITH("Faith", 4, 0x80),
+    INNOCENT("Innocent", 4, 0x40),
+    CHARM("Charm", 4, 0x20),
+    SLEEP("Sleep", 4, 0x10),
+    DONT_MOVE("Don't Move", 4, 0x08),
+    DONT_ACT("Don't Act", 4, 0x04),
+    REFLECT("Reflect", 4, 0x02),
+    DEATH_SENTENCE("Death Sentence", 4, 0x01);
 
+    private final String displayName;
     private final int offset;
     private final int flag;
 
-    Status(final int offset, final int flag) {
-        this.offset = (byte) offset;
-        this.flag = (byte) flag;
+    Status(final String displayName, final int offset, final int flag) {
+        this.displayName = displayName;
+        this.offset = offset;
+        this.flag = flag;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
     public int getOffset() {
